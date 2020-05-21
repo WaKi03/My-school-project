e1b4344f00d35bf23d7c628c2a1df743bd9da9e1
WaKi03 <Vladiosos03@gmail.com>
2020-05-21 12:41:11 +0400

    1st

---
+++ rup4ik/app/src/main/java/com/vladiosos03/rup4ik/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vladiosos03.rup4ik;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
