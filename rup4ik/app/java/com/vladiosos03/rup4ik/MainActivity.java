e24a69c565380bf9c078c517a3b4c54e3773065c
WaKi03 <vladiosos03@gmail.com>
2021-03-28 19:38:04 +0400

    Android studio. Вроде готова одна страница

--- rup4ik/app/java/com/vladiosos03/rup4ik/MainActivity.java
+++ rup4ik/app/java/com/vladiosos03/rup4ik/MainActivity.java
@@ -18,15 +18,36 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
         // получаем все поля по id из main.xml
         resultField = (TextView) findViewById(R.id.sText);//результат в графе s =
-        aField = (EditText) findViewById(R.id.aText);//ввод данныз в поле a =
-        timeField = (EditText) findViewById(R.id.tText); //ввод данныз в поле t =
-        vField = (EditText) findViewById(R.id.vText);//ввод данныз в поле v =
+
         // Инициализация^^^^^
+
+        Button resButton;
+        resButton = (Button) findViewById(R.id.resButton);
+        // создаем обработчик нажатия
+        View.OnClickListener resButtonListener = v -> {
+            aField = (EditText) findViewById(R.id.aText);//ввод данныз в поле a =
+            timeField = (EditText) findViewById(R.id.tText); //ввод данныз в поле t =
+            vField = (EditText) findViewById(R.id.vText);//ввод данныз в поле v =
+
+            int a = Integer.parseInt(aField.getText().toString()); //конвертируем все введеные значения в int
+            int t = Integer.parseInt(timeField.getText().toString());
+            int v0 = Integer.parseInt(vField.getText().toString());
+
+            int s = v0*t+((a*t*t)/2);
+
+            resultField.setText("Ответ: " + s);
+
+        };
+        resButton.setOnClickListener(resButtonListener);
+
+
+
+
 
         Button closeButton;
         closeButton = (Button) findViewById(R.id.button2);
         // создаем обработчик нажатия
-        View.OnClickListener closeButtonListener = v ->   Toast.makeText(this, "не выходите из приложения, пожалуйста", Toast.LENGTH_LONG).show();;
+        View.OnClickListener closeButtonListener = v ->   Toast.makeText(this, "не выходите из приложения, пожалуйста", Toast.LENGTH_LONG).show();
         closeButton.setOnClickListener(closeButtonListener);
         }
 }
